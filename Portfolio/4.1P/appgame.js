78100edd252c8524210f02771bbc7189fb7cb1ea
luongtracducanh <luongtrac.ducanh@gmail.com>
2023-05-29 13:46:11 +0700

    added new files

---
+++ Portfolio/4.1P/appgame.js
@@ -0,0 +1,35 @@
+Vue.createApp({
+  data() {
+    return {
+      guess: 0,
+      secret: 0,
+      msg: "",
+    };
+  },
+  methods: {
+    genRandomNum: function () {
+      return Math.floor(Math.random() * 100) + 1;
+    },
+    checkGuess: function () {
+      if (this.guess == this.secret) {
+        this.msg = "You got it!";
+      } else if (this.guess > this.secret) {
+        this.msg = "Guess lower.";
+      } else {
+        this.msg = "Guess higher.";
+      }
+    },
+    giveUp: function () {
+      this.msg = "The number was " + this.secret + ".";
+    },
+    startOver: function () {
+      this.secret = this.genRandomNum();
+      this.guess = 0;
+      this.msg = "Start guessing.";
+    },
+  },
+  created: function () {
+    this.secret = this.genRandomNum();
+    this.msg = "Start guessing.";
+  },
+}).mount("#app");
