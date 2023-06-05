ff37d668efbf1cdf7cf9f3b34a97f2e906e028d6
luongtracducanh <luongtrac.ducanh@gmail.com>
2023-06-05 16:20:44 +0700

    added new files

---
+++ Portfolio/5.3C/applookup2.js
@@ -0,0 +1,132 @@
+// Defining the list of units in an array
+var units = [
+  {
+    code: "ICT10001",
+    desc: "Problem Solving with ICT",
+    cp: 12.5,
+    type: "Core",
+  },
+  { code: "COS10005", desc: "Web Development", cp: 12.5, type: "Core" },
+  {
+    code: "INF10003",
+    desc: "Introduction to Business Information Systems",
+    cp: 12.5,
+    type: "Core",
+  },
+  {
+    code: "INF10002",
+    desc: "Database Analysis and Design",
+    cp: 12.5,
+    type: "Core",
+  },
+  {
+    code: "COS10009",
+    desc: "Introduction to Programming",
+    cp: 12.5,
+    type: "Core",
+  },
+  {
+    code: "INF30029",
+    desc: "Information Technology Project Management",
+    cp: 12.5,
+    type: "Core",
+  },
+  {
+    code: "ICT30005",
+    desc: "Professional Issues in Information Technology",
+    cp: 12.5,
+    type: "Core",
+  },
+  {
+    code: "ICT30001",
+    desc: "Information Technology Project",
+    cp: 12.5,
+    type: "Core",
+  },
+];
+
+// Creating a component for the units to pass in the router
+const Unit = {
+  props: ["id"],
+  data() {
+    return { units };
+  },
+  // Define the template for the route results
+  template: `
+    <div>
+      <h2>Unit Code: {{ $route.params.id }}</h2>
+      <ul v-for="unit in filteredUnits">
+        <li>{{ unit.code }}</li>
+        <li>{{ unit.desc }}</li>
+        <li>{{ unit.cp }}</li>
+        <li>{{ unit.type }}</li>
+      </ul>
+    </div>
+  `,
+  computed: {
+    // Filter function (returns the selected unit object)
+    filteredUnits() {
+      return this.units.filter((unit) =>
+        unit.code.toLowerCase().match(this.$route.params.id.toLowerCase())
+      );
+    },
+  },
+};
+
+// Creating the VueRouter
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHashHistory(),
+  routes: [
+    {
+      path: "/unit/:id",
+      component: Unit,
+      //defining path and the component
+    },
+  ],
+});
+
+// Create a new app instance
+const app = Vue.createApp({});
+
+// Creating component for the lookup table
+app.component("app-lookup2", {
+  data() {
+    return {
+      units,
+    };
+  },
+  // Defining template for the app
+  template: `
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="table-responsive">
+          <table class="table table-striped table-hover">
+            <thead>
+              <tr>
+                <th>Code</th>
+                <th>Description</th>
+                <th>More Info</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="unit in units" :key="unit.code">
+                <td>{{ unit.code }}</td>
+                <td>{{ unit.desc }}</td>
+                <td><router-link :to="{ path: '/unit/' + unit.code }">Read More</router-link></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12">
+        <router-view></router-view>
+      </div>
+    </div>
+  `,
+});
+
+// Use router and mount to app
+app.use(router);
+app.mount("#app");
--- Resources/Week 5 Sample Code-1/js/route_with_param.js
+++ Resources/Week 5 Sample Code-1/js/route_with_param.js
@@ -1,25 +1,28 @@
 //Defining the list of subjects in an array
-var subjects = [{
-    code: 'Math',
-    desc: 'We learn Mathematics in this unit.' 
+var subjects = [
+  {
+    code: "Math",
+    desc: "We learn Mathematics in this unit.",
   },
   {
-    code: 'English',
-    desc: 'There are lots of fun classes' 
+    code: "English",
+    desc: "There are lots of fun classes",
   },
   {
-    code: 'Dancing',
-    desc: 'Dancing classes with excellent teachers' 
-  }
+    code: "Dancing",
+    desc: "Dancing classes with excellent teachers",
+  },
 ];
 
 // creating a component to pass in the router
 const Subject = {
-	data: function() {  return {    subjects    }  },
+  data: function () {
+    return { subjects };
+  },
 
-	//A dynamic segment is denoted by a colon :   When a route is matched, the value of the dynamic segments will be exposed as this.$route.params
-	
-	template: `<div>
+  //A dynamic segment is denoted by a colon :   When a route is matched, the value of the dynamic segments will be exposed as this.$route.params
+
+  template: `<div>
 		<hr>
 		<h2>Details of {{ $route.params.id }}</h2> 
 		<ul v-for="unit in filteredUnits">
@@ -28,34 +31,34 @@ const Subject = {
 		</ul>
 	</div>`,
 
-	computed: {
-		//returns the selected subjects 
-		filteredUnits: function() {
-		return this.subjects.filter(s => s.code.toLowerCase().match((this.$route.params.id.toLowerCase())));
-		}
-	}
-}
+  computed: {
+    //returns the selected subjects
+    filteredUnits: function () {
+      return this.subjects.filter((s) =>
+        s.code.toLowerCase().match(this.$route.params.id.toLowerCase())
+      );
+    },
+  },
+};
 
 // Creating the Router
 const router = VueRouter.createRouter({
-	history: VueRouter.createWebHashHistory(),
-	
-	routes: [
-		{ path: '/subject/:id',  component: Subject }
-    ] 	//defining path and component mapping
-})
+  history: VueRouter.createWebHashHistory(),
+
+  routes: [{ path: "/subject/:id", component: Subject }], //defining path and component mapping
+});
 
 //Creating new Vue instance
-const app = Vue.createApp({})
+const app = Vue.createApp({});
 
 // creating component for the lookup table
-app.component('example_component', {
-	data: function() { 
-		return {   subjects    }
-	},
-	
-	//defining template  
-	template: `
+app.component("example_component", {
+  data: function () {
+    return { subjects };
+  },
+
+  //defining template
+  template: `
 		<div>
 			<h2>Subject Information</h2>
 			<table >
@@ -72,8 +75,8 @@ app.component('example_component', {
 			</table>
 			
 			<router-view ></router-view>		
-		</div> `
-})
+		</div> `,
+});
 
-app.use(router)
-app.mount('#app')
+app.use(router);
+app.mount("#app");
