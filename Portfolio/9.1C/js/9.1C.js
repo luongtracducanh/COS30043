846e02de2c2e4df372669f6895751e2518239e0b
luongtracducanh <luongtrac.ducanh@gmail.com>
2023-06-28 13:15:38 +0700

    added new files

---
+++ Portfolio/9.1C/js/9.1C.js
@@ -0,0 +1,75 @@
+// Create VueRouter instance
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHashHistory(),
+  routes: [
+    {
+      path: "/login",
+      component: Login,
+      name: "login",
+    },
+    {
+      path: "/logout",
+      name: "logout",
+    },
+    {
+      path: "/dashboard",
+      component: Dashboard,
+      name: "dashboard",
+    },
+  ],
+});
+
+// Create Vue instance
+const app = Vue.createApp({
+  data: function () {
+    return {
+      authenticated: false,
+      authenticatedUser: "",
+      error: false,
+      errorMsg: "",
+    };
+  },
+  mounted() {
+    if (!this.authenticated) {
+      this.$router.replace({ name: "login" });
+    }
+  },
+  methods: {
+    setAuthenticated(status) {
+      this.authenticated = status;
+    },
+    logout() {
+      this.authenticated = false;
+    },
+  },
+});
+
+// Create components
+app.component("app-nav", {
+  template: `
+	<div>
+    <v-spacer></v-spacer>
+    <v-btn>
+        <router-link  to="/login" v-on:click="logout()" replace>Logout<v-icon>mdi-logout</v-icon></router-link>
+		          <!-- replace: the navigation will not leave a history record.  -->
+    </v-btn>
+    </div>
+  `,
+  methods: {
+    logout() {
+      this.$root.logout();
+    },
+  },
+});
+
+// Register components
+app.component("app-read", ReadData);
+app.component("app-post", PostData);
+app.component("app-update", UpdateData);
+app.component("app-delete", DeleteData);
+
+// Create Vuetify instance and use it
+const vuetify = Vuetify.createVuetify();
+app.use(vuetify);
+app.use(router);
+app.mount("#app");
---
+++ Portfolio/9.1C/js/components/app-dashboard.js
@@ -0,0 +1,35 @@
+const Dashboard = {
+  // defining data to be used in the component
+  data: function () {
+    return {
+      tab: null,
+      items: ["View", "Insert", "Update", "Delete"],
+    };
+  },
+
+  // define the template for the component
+  template: `
+  <div id="dashboard">
+    <v-card>
+      <v-card-title class="text-center justify-center py-6">
+        <h1 class="font-weight-bold display-3 basil--text">Dashboard</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-tabs v-model="tab" background-color="transparent" color="basil" grow>
+          <v-tab v-for="item in items" :key="item"> {{ item }} </v-tab>
+        </v-tabs>
+        <v-tabs-items v-model="tab">
+          <v-tab-item>
+            <v-card flat>
+              <app-read v-if="tab==0"></app-read>
+              <app-post v-show="tab==1"></app-post>
+              <app-update v-show="tab==2"></app-update>
+              <app-delete v-show="tab==3"></app-delete>
+            </v-card>
+          </v-tab-item>
+        </v-tabs-items>
+      </v-card-text>
+    </v-card>
+  </div>
+  `,
+};
